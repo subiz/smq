e5d835f6ca6c26bffd6c118f5708fc8142c48c72
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-01-29 18:53:49 +0700

    fix db

--- queuedb.go
+++ queuedb.go
@@ -9,13 +9,14 @@ import (
 )
 
 const (
-	tblIndices = "indices"
-	tableJobs  = "jobs"
+	tblOffsets = "offsets"
+	tblQueues  = "queues"
 )
 
 type SMQ interface {
 	Enqueue(partition int, queue string, value []byte) (int64, error)
 
+	// Commit updates consumer offset for a queue
 	Commit(partition int, queue string, index int64) error
 
 	// the latter paramteter is index of the last message
@@ -25,6 +26,10 @@ type SMQ interface {
 const GROUP_SIZE = 1000
 
 // QueueDB manages subscription for webhook
+// CREATE KEYSPACE smqtest WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1};
+// CREATE TABLE queues(queue ASCII,partition INT,group BIGINT,offset BIGINT,data BLOB, PRIMARY KEY ((queue, partition, group), offset)) WITH CLUSTERING ORDER BY (offset ASC);
+// CREATE TABLE offsets(queue ASCII, partition INT,consumer_offset BIGINT,producer_offset BIGINT,PRIMARY KEY ((queue, partition)));
+//
 type QueueDB struct {
 	session *gocql.Session
 	l       *cache.Cache
@@ -63,18 +68,23 @@ func NewQueueDB(seeds []string, ks string) (*QueueDB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return nil, err
+	return me, err
 }
 
-func (me *QueueDB) Fetch(partition int, queue string, offset int64) ([][]byte, int64, error) {
-	group := offset / GROUP_SIZE
-	query := `SELECT offset, value FROM ` + tableJobs +
-		` WHERE partition=? AND queue=? AND group=? AND offset>? LIMIT ?`
+func (me *QueueDB) Fetch(partition int, queue string) ([][]byte, int64, error) {
+	initoffset, maxoffset, err := me.readIndex(partition, queue)
+	if err != nil {
+		return nil, -1, err
+	}
+
+	group := initoffset / GROUP_SIZE
+	query := `SELECT offset, data FROM ` + tblQueues +
+		` WHERE partition=? AND queue=? AND group=? AND offset>? ORDER BY offset ASC LIMIT ?`
 
 	valueArr := make([][]byte, 0)
 	value := make([]byte, 0)
 	var lastoffset int64
-	iter := me.session.Query(query, partition, queue, group, offset).Iter()
+	iter := me.session.Query(query, partition, queue, group, initoffset, GROUP_SIZE).Iter()
 	for iter.Scan(&lastoffset, &value) {
 		valueArr = append(valueArr, value)
 		value = make([]byte, 0)
@@ -84,8 +94,9 @@ func (me *QueueDB) Fetch(partition int, queue string, offset int64) ([][]byte, i
 	}
 
 	// out of current group (not group out of message)
-	if lastoffset+10%GROUP_SIZE > group {
-		iter := me.session.Query(query, partition, queue, group+1, offset).Iter()
+	if len(valueArr) < GROUP_SIZE && lastoffset < maxoffset {
+		iter := me.session.Query(query, partition, queue, group+1, lastoffset,
+			GROUP_SIZE-len(valueArr)).Iter()
 		for iter.Scan(&lastoffset, &value) {
 			valueArr = append(valueArr, value)
 			value = make([]byte, 0)
@@ -99,32 +110,31 @@ func (me *QueueDB) Fetch(partition int, queue string, offset int64) ([][]byte, i
 }
 
 func (me *QueueDB) readIndex(partition int, queue string) (csm, pro int64, err error) {
-	csmi, csmok := me.l.Get(fmt.Sprintf("producer-%d-%s", partition, queue))
+	csmi, csmok := me.l.Get(fmt.Sprintf("consumer-%d-%s", partition, queue))
 	proi, prook := me.l.Get(fmt.Sprintf("producer-%d-%s", partition, queue))
 	if csmok && prook {
 		return csmi.(int64), proi.(int64), nil
 	}
 
-	query := "SELECT consumer_offset, producer_offset FROM " + tblIndices +
+	query := "SELECT consumer_offset, producer_offset FROM " + tblOffsets +
 		" WHERE partition=? AND queue=?"
 
 	err = me.session.Query(query, partition, queue).Scan(&csm, &pro)
-	if err != nil && err.Error() != gocql.ErrNotFound.Error() {
-		me.l.Add(fmt.Sprintf("consumer-%d-%s", partition, queue), 0)
-		me.l.Add(fmt.Sprintf("consumer-%d-%s", partition, queue), 0)
+	if err != nil && err.Error() == gocql.ErrNotFound.Error() {
+		me.l.Add(fmt.Sprintf("consumer-%d-%s", partition, queue), int64(0))
+		me.l.Add(fmt.Sprintf("producer-%d-%s", partition, queue), int64(0))
 		return 0, 0, nil
 	}
 	if err != nil {
-
 		return -1, -1, errors.Wrap(err, 500, errors.E_database_error)
 	}
 	me.l.Add(fmt.Sprintf("consumer-%d-%s", partition, queue), csm)
-	me.l.Add(fmt.Sprintf("consumer-%d-%s", partition, queue), pro)
+	me.l.Add(fmt.Sprintf("producer-%d-%s", partition, queue), pro)
 	return csm, pro, nil
 }
 
 func (me *QueueDB) updateProducerIndex(partition int, queue string, offset int64) error {
-	err := me.session.Query("INSERT INTO "+tblIndices+
+	err := me.session.Query("INSERT INTO "+tblOffsets+
 		" (partition, queue, producer_offset) VALUES(?,?,?)", partition, queue, offset).Exec()
 	if err != nil {
 		return errors.Wrap(err, 500, errors.E_database_error)
@@ -134,7 +144,7 @@ func (me *QueueDB) updateProducerIndex(partition int, queue string, offset int64
 }
 
 func (me *QueueDB) updateConsumerIndex(partition int, queue string, offset int64) error {
-	err := me.session.Query("INSERT INTO "+tblIndices+
+	err := me.session.Query("INSERT INTO "+tblOffsets+
 		" (partition, queue, consumer_offset) VALUES(?,?,?)", partition, queue, offset).Exec()
 	if err != nil {
 		return errors.Wrap(err, 500, errors.E_database_error)
@@ -155,7 +165,7 @@ func (me *QueueDB) Enqueue(partition int, queue string, value []byte) (int64, er
 		return -1, err
 	}
 
-	query := "INSERT INTO " + tableJobs + "(partition, queue, group, offset, value) " +
+	query := "INSERT INTO " + tblQueues + "(partition, queue, group, offset, data) " +
 		"VALUES(?,?,?,?,?)"
 	group := offset / GROUP_SIZE
 	err = me.session.Query(query, partition, queue, group, offset, value).Exec()
@@ -166,7 +176,6 @@ func (me *QueueDB) Enqueue(partition int, queue string, value []byte) (int64, er
 	return offset, nil
 }
 
-// Commit job
 func (me *QueueDB) Commit(partition int, queue string, offset int64) error {
 	csm, pro, err := me.readIndex(partition, queue)
 	if err != nil {
